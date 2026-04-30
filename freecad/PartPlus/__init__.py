0174dd6a243b479c6d43c21e69eb027eebd7ab96
FC-FBXL5 <103560699+FC-FBXL5@users.noreply.github.com>
2026-04-30 14:43:45 +0200

    Create __init__.py

---
+++ freecad/PartPlus/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the PartPlus addon.
+# -*- coding: utf-8 -*-
+
+from FreeCAD import Console
+
+Console.PrintLog('freecad/PartPlus/__init__.py\n')
